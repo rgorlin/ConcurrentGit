426bc4575714914e6c06b41c191c2cc9ac118b8d
Steven G <reuven9899@gmail.com>
2019-11-19 18:15:35 -0500

    added hello world

---
+++ HelloWorld/src/class1.java
@@ -0,0 +1,12 @@
+/**
+ * this is so so much fun!!
+ */
+public class class1 {
+    /**
+     * good comments
+     * @param args :()
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
